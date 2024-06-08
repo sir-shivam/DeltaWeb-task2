c5e38c34ca431fbf06994ed379ddfbe6fb17749c
sir-shivam <sirshivam95@gmail.com>
2024-06-08 17:33:27 +0530

    Added some event listeners

--- game.js
+++ game.js
@@ -5,7 +5,9 @@ let canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth -7.1;
 canvas.height = window.innerHeight -7.1;
 
+let lastKey;
 let c = canvas.getContext("2d");
+let gravity = 0.2;
 
 // let height=150;
 class character {
@@ -22,11 +24,12 @@ class character {
 
     update() {
         this.draw();
+        this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
 
         if(this.position.y + this.height + this.velocity.y >= canvas.height){
             this.velocity.y = 0;
-        }
+        } else this.velocity.y += gravity; 
     }
 }
 
@@ -37,7 +40,7 @@ const player = new character({
     },
     velocity: {
         x: 0,
-        y: 10
+        y: 0
     }
 })
 
@@ -50,11 +53,23 @@ const enemy = new character({
     },
     velocity: {
         x: 0,
-        y: 5
+        y: 0
     }
 })
 
 // enemy.draw();
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    w:{
+        pressed: false
+    }
+
+}
 
 function animate (){
     window.requestAnimationFrame(animate);
@@ -64,6 +79,43 @@ function animate (){
     c.clearRect(0, 0, canvas.width, canvas.height);
     player.update();
     enemy.update();
+
+    player.velocity.x = 0;
+
+    if (keys.a.pressed && lastKey == "a") {
+        player.velocity.x = -1;
+    }
+    else if (keys.d.pressed && lastKey == "d") {
+        player.velocity.x = 1;
+    }
 }
 
 animate();
+
+window.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case 'd': 
+            keys.d.pressed = true;
+            lastKey = "d";
+            break
+        case 'a':
+            keys.a.pressed = true;
+            lastKey = "a";
+            break
+        case 'w':
+            keys.w.pressed = true;
+            lastKey = "w";
+            break
+    }
+})
+
+window.addEventListener("keyup", (event) => {
+    switch (event.key) {
+        case 'd':
+            // keys.d.pressed = false; 
+            break
+        case 'a':
+            // keys.a.pressed = false;
+            break;
+    } 
+})
