991925b12def3f30752d8c2c8937fe155aa1e1c8
sir-shivam <sirshivam95@gmail.com>
2024-06-11 20:24:22 +0530

    created the blocks

--- game.js
+++ game.js
@@ -5,9 +5,8 @@ let canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth -7.1;
 canvas.height = window.innerHeight -7.1;
 
-let lastKey;
 let c = canvas.getContext("2d");
-let gravity = 0.2;
+let gravity = 0.5;
 
 // let height=150;
 class character {
@@ -15,11 +14,19 @@ class character {
         this.position = position;
         this.velocity = velocity;
         this.height = 150;
+        this.lastKey;
+        this.box = {
+            position: this.position,
+            width: 100,
+            height: 50
+        }
     }
 
     draw() {
         c.fillStyle = "red";
         c.fillRect(this.position.x , this.position.y ,50 , this.height)
+        // c.fillStyle = "green"
+        c.fillRect(this.box.position.x, this.box.position.y, this.box.width, this.box.height)
     }
 
     update() {
@@ -29,14 +36,61 @@ class character {
 
         if(this.position.y + this.height + this.velocity.y >= canvas.height){
             this.velocity.y = 0;
-        } else this.velocity.y += gravity; 
+        } 
+        else this.velocity.y += gravity; 
+    }
+}
+
+class blocks {
+    constructor({position , velocity}){
+        this.position = position;
+        this.velocity = velocity;
+        this.height = 100;
+        this.width = 100;
+    }
+
+    draw() {
+        c.fillStyle = "pink";
+        c.strokeStyle = "black";
+        c.stroke();
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+        // console.log("done");
+    }
+
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+        if(this.position.y + this.height + this.velocity.y >= canvas.height){
+            this.velocity.y = 0;
+        } 
+        else this.velocity.y += gravity; 
+    }
+}
+
+class gun {
+    constructor({position}) {
+        this.position = position;
+        this.width = 100;
+        this.height = 20;
+    }
+
+    draw(){
+        c.fillStyle = "green";
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    update() {
+        this.draw();
+        // this.position.x += this.velocity.x;
+        // this.position.y += this.velocity.y;
     }
 }
 
 const player = new character({
     position: {
-    x: 0,
-    y: 0
+        x: window.innerWidth/2,
+        y: 0
     },
     velocity: {
         x: 0,
@@ -44,7 +98,53 @@ const player = new character({
     }
 })
 
-// player.draw();
+const gun1 = new gun ({
+    position: {
+        x: player.position.x + 50,
+        y: 600
+    }
+})
+
+const box1 = new blocks ({
+    position: {
+        x: player.position.x + 200,
+        y: 0 
+    },
+    velocity: {
+        x: player.velocity.x,
+        y: player.velocity.y
+    }
+})
+const box2 = new blocks ({
+    position: {
+        x: player.position.x + 220,
+        y: -200
+    },
+    velocity: {
+        x: player.velocity.x,
+        y: player.velocity.y
+    }
+})
+const box3 = new blocks ({
+    position: {
+        x: player.position.x - 200,
+        y: 0 
+    },
+    velocity: {
+        x: player.velocity.x,
+        y: player.velocity.y
+    }
+})
+const box4 = new blocks ({
+    position: {
+        x: player.position.x - 220,
+        y: -200
+    },
+    velocity: {
+        x: player.velocity.x,
+        y: player.velocity.y
+    }
+})
 
 const enemy = new character({
     position: {
@@ -68,7 +168,6 @@ const keys = {
     w:{
         pressed: false
     }
-
 }
 
 function animate (){
@@ -78,24 +177,31 @@ function animate (){
     // c.fillRect(0 , 0, canvas.width, canvas.height);
     c.clearRect(0, 0, canvas.width, canvas.height);
     player.update();
-    enemy.update();
-
+    gun1.update();
+    box1.update();
+    box2.update();
+    box3.update();
+    box4.update();
+    // enemy.update();
+    
     player.velocity.x = 0;
 
     if (keys.a.pressed && lastKey == "a") {
-        player.velocity.x = -1;
+        player.velocity.x = -5;
     }
     else if (keys.d.pressed && lastKey == "d") {
-        player.velocity.x = 1;
+        player.velocity.x = 5;
+        }
     }
-}
-
+    
+    
 animate();
 
 window.addEventListener("keydown", (event) => {
     switch (event.key) {
         case 'd': 
             keys.d.pressed = true;
+            console.log("call");
             lastKey = "d";
             break
         case 'a':
@@ -104,18 +210,22 @@ window.addEventListener("keydown", (event) => {
             break
         case 'w':
             keys.w.pressed = true;
-            lastKey = "w";
+            player.velocity.y = -18;
             break
     }
 })
 
 window.addEventListener("keyup", (event) => {
+    console.log("haaaa");
     switch (event.key) {
         case 'd':
-            // keys.d.pressed = false; 
+            keys.d.pressed = false;
+            console.log("Hiiiiii");
+            player.velocity.x = 0; 
             break
         case 'a':
-            // keys.a.pressed = false;
+            keys.a.pressed = false;
+            player.velocity.x = 0;
             break;
     } 
 })
