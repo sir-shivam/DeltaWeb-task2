48b605e0168f473efafa04795cfac2d84491ca83
sir-shivam <sirshivam95@gmail.com>
2024-06-13 21:48:27 +0530

    collission detected

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ canvas.width = window.innerWidth -7.1;
 canvas.height = window.innerHeight -7.1;
 
 let c = canvas.getContext("2d");
-let gravity = 0.5;
+let gravity = 0.8;
 let bullets = [];
 
 // let height=150;
@@ -15,19 +15,20 @@ class character {
         this.position = position;
         this.velocity = velocity;
         this.height = 150;
+        this.width = 50;
         this.lastKey;
-        this.box = {
-            position: this.position,
-            width: 100,
-            height: 50
-        }
+        // this.box = {
+        //     position: this.position,
+        //     width: 100,
+        //     height: 50
+        // }
     }
 
     draw() {
         c.fillStyle = "red";
-        c.fillRect(this.position.x , this.position.y ,50 , this.height)
+        c.fillRect(this.position.x , this.position.y ,this.width, this.height)
         // c.fillStyle = "green"
-        c.fillRect(this.box.position.x, this.box.position.y, this.box.width, this.box.height)
+        // c.fillRect(this.box.position.x, this.box.position.y, this.box.width, this.box.height)
     }
 
     update() {
@@ -85,46 +86,48 @@ class gun {
         c.translate(-this.pivote.x, -this.pivote.y);
         c.fillStyle = "green";
         c.fillRect(this.position.x, this.position.y, this.width, this.height);
-
-        for (let i = bullets.length - 1; i >= 0; i--) {
-            const bullet = bullets[i];
-            bullet.update();
+        c.restore();
+        
+        bullets.forEach( bullet => {
+            bullet.move();
             bullet.draw();
+
             if (bullet.x < 0 || bullet.x > canvas.width || bullet.y < 0 || bullet.y > canvas.height) {
                 bullets.splice(i, 1);
-              }
+               }
+            else if(collide(bullet)){
+                bullets.splice(i, 1);
             }
-        c.restore();
+        })
     }
 
     update() {
         this.draw();
-        // this.position.x += this.velocity.x;
-        // this.position.y += this.velocity.y;
     }
 }
+
 let i=0;
 class bullet {
-    constructor(x, y, angle, velY) {
+    constructor(x, y, angle) {
         this.x = x;
         this.y = y;
-        this.velY= velY;
-        this.angle = angle;
-        this.speed = 5;
+        this.angle = angle;    
+        this.dx = Math.cos(angle) * 20;
+        this.dy = Math.sin(angle) * 20;
     }
+
+    move() {
+        this.x += this.dx;
+        this.y += this.dy;
+        if(this.y <= canvas.height){
+            this.dy += gravity;
+        } 
+    }
+
 
     draw() {
         c.fillStyle = 'black';
         c.fillRect(this.x - 2, this.y - 2, 4, 4);
-    }
-    
-    update() {
-        if(this.y + this.height + this.velY >= canvas.height){
-                this.velY = 0;
-            } 
-            else this.velY += gravity;
-            // this.y += 20;//* Math.sin(this.angle);
-            this.x += this.speed; //* Math.cos(this.angle);
     }
 }
 
@@ -191,18 +194,6 @@ const box4 = new blocks ({
     }
 })
 
-const enemy = new character({
-    position: {
-    x: 400,
-    y: 100
-    },
-    velocity: {
-        x: 0,
-        y: 0
-    }
-})
-
-// enemy.draw();
 const keys = {
     a: {
         pressed: false
@@ -217,9 +208,6 @@ const keys = {
 
 function animate (){
     window.requestAnimationFrame(animate);
-    console.log("working");
-    // c.fillStyle = "wheat";
-    // c.fillRect(0 , 0, canvas.width, canvas.height);
     c.clearRect(0, 0, canvas.width, canvas.height);
     player.update();
     gun1.update();
@@ -227,7 +215,6 @@ function animate (){
     box2.update();
     box3.update();
     box4.update();
-    // enemy.update();
     
     player.velocity.x = 0;
 
@@ -237,26 +224,106 @@ function animate (){
     else if (keys.d.pressed && lastKey == "d") {
         player.velocity.x = 5;
         }
+
+    if(box2.position.y + box2.height >= box1.position.y){
+        box2.velocity.y = 0; 
     }
+
+    if(box4.position.y + box4.height >= box3.position.y){
+        box4.velocity.y = 0; 
+    }
+
+    if (playerCollide()){
+        console.log("touch");
+        // player.position.x = player.position.x;
+        player.velocity.y =0;
+        player.velocity.x = 0;
+    }
+}
     
     
 animate();
 
 
 function fireBullet() {
-    let bulletX = gun1.position.x + gun1.width //* Math.cos(gun1.rotationAngle * Math.PI / 180);
-    let bulletY = gun1.position.y + gun1.height / 2 //* Math.sin(gun1.rotationAngle * Math.PI / 180);
-    let velY = gun1.position.y + 0;
-    // let bulletAngle = gun1.rotationAngle * Math.PI / 180;
-    bullets.push(new bullet(bulletX, bulletY,velY))
+    distance = 45 + gun1.width;
+    let angle = gun1.rotationAngle * Math.PI / 180;
+    let delX = distance * Math.cos(angle);
+    let delY = distance * Math.sin(angle);
+    let bulletX = gun1.pivote.x + delX;
+    let bulletY = gun1.pivote.y + delY;
+    
+    console.log(angle , gun1.rotationAngle);
+    bullets.push(new bullet(bulletX, bulletY, angle))
 }
 
+function collide(bullet) {
+    let test1 = 
+       (bullet.x >= box4.position.x &&
+        bullet.x <= box4.position.x + box4.height &&
+        bullet.y >= box4.position.y &&
+        bullet.y <= box4.position.y + box4.width) 
+    
+    let test2 = 
+       (bullet.x >= box3.position.x &&
+        bullet.x <= box3.position.x + box4.height &&
+        bullet.y >= box3.position.y &&
+        bullet.y <= box3.position.y + box4.width) 
+    
+    let test3 = 
+       (bullet.x >= box1.position.x &&
+        bullet.x <= box1.position.x + box4.height &&
+        bullet.y >= box1.position.y &&
+        bullet.y <= box1.position.y + box4.width) 
+    
+    let test4 = 
+        (bullet.x >= box2.position.x &&
+        bullet.x <= box2.position.x + box4.height &&
+        bullet.y >= box2.position.y &&
+        bullet.y <= box2.position.y + box4.width) 
+
+    return (test1 || test2 || test3 || test4);
+}
+
+function playerCollide() {
+    let test1 = 
+    (   player.position.x + player.width >= box1.position.x &&
+        player.position.x + player.width <= box1.position.x + box1.width &&
+        player.position.y + player.height >= box1.position.y &&
+        player.position.y + player.height <= box1.position.y + box1.height
+        
+    )
+    
+    let test2 = 
+    (   player.position.x + player.width >= box2.position.x &&
+        player.position.x + player.width <= box2.position.x + box2.width &&
+        player.position.y + player.height >= box2.position.y &&
+        player.position.y + player.height <= box2.position.y + box2.height
+        
+    )
+    let test3 = 
+    (   player.position.x  >= box3.position.x &&
+        player.position.x  <= box3.position.x + box3.width &&
+        player.position.y + player.height >= box3.position.y &&
+        player.position.y + player.height <= box3.position.y + box3.height
+        
+    )
+    let test4 = 
+    (   player.position.x  >= box4.position.x &&
+        player.position.x  <= box4.position.x + box4.width &&
+        player.position.y + player.height >= box4.position.y &&
+        player.position.y + player.height <= box4.position.y + box4.height
+        
+    )
+
+    return(test1 || test2 || test3 || test4);
+
+}
 
 window.addEventListener("keydown", (event) => {
     switch (event.key) {
         case 'd': 
             keys.d.pressed = true;
-            console.log("call");
             lastKey = "d";
             break
         case 'a':
@@ -268,10 +335,10 @@ window.addEventListener("keydown", (event) => {
             player.velocity.y = -18;
             break
         case 'ArrowLeft':
-            gun1.rotationAngle -= 5; 
+            gun1.rotationAngle -= 4; 
             break;
         case 'ArrowRight':
-            gun1.rotationAngle += 5;
+            gun1.rotationAngle += 4;
             break;
         case 'f':
             fireBullet();
@@ -280,11 +347,9 @@ window.addEventListener("keydown", (event) => {
 })
 
 window.addEventListener("keyup", (event) => {
-    console.log("haaaa");
     switch (event.key) {
         case 'd':
             keys.d.pressed = false;
-            console.log("Hiiiiii");
             player.velocity.x = 0; 
             break
         case 'a':
@@ -294,5 +359,4 @@ window.addEventListener("keyup", (event) => {
     } 
 })
 
-
-  
+window.addEventListener("click" , fireBullet )
